5fbbeff27e3e698d7ceb43a2046fc44f1337f165
AlexZheng82 <alexz82@nycstudents.net>
2024-04-09 11:45:13 -0400

    Hello

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import json
+import os
 ## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-
+game_life=100
+menu_location="starting"
+while game_life>=1:
+    if menu_location=="starting":
+        print("TITLE!!!!!!")
+        print("1. Start game")
+        print("2. End game")
+        player_choice=input("input: ")
+    os.system('cls')

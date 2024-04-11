46d8a409c7be5799931fd1ebf88b88f0dee6d522
hongenl <hongenl@nycstudents.net>
2024-04-11 12:34:34 -0400

    Create enemy.py

---
+++ enemy.py
@@ -0,0 +1,10 @@
+
+
+class enemy():
+    def __init__(self, health, attack):
+        self.health = health
+        self.attack = attack
+
+class ingredient(enemy):
+    def seasoningenemy():
+        

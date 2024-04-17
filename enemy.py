8e55f987972bedc423b0e7685e7c36ad0f199106
hongenl <hongenl@nycstudents.net>
2024-04-17 12:28:47 -0400

    e

--- enemy.py
+++ enemy.py
@@ -1,15 +1,16 @@
 enemies = []
 
 class enemy():
-    def __init__(self, name, health, attack):
+    def __init__(self, name, health, attack, moveset):
         self.name = name
         self.health = health
         self.attack = attack
+        self.moveset = moveset
 
 class ingredient(enemy):
-    def __init__(self, health, attack, drops):
-        super().__init__(health, attack)
+    def __init__(self, health, attack, moveset, drops):
+        super().__init__(health, attack, moveset)
         self.drops = drops
     def __str__(self):
         return f"{self.name}, {self.health}, {self.attack}"
-    
+

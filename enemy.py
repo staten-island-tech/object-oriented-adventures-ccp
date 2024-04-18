2aaad8e1a020b9382bc38bd898b5339459d7de33
hongenl <hongenl@nycstudents.net>
2024-04-18 12:34:20 -0400

    EE

--- enemy.py
+++ enemy.py
@@ -1,16 +1,49 @@
-enemies = []
+import json
+import os
 
-class enemy():
+class typeenemy():
     def __init__(self, name, health, attack, moveset):
         self.name = name
         self.health = health
         self.attack = attack
         self.moveset = moveset
 
-class ingredient(enemy):
-    def __init__(self, health, attack, moveset, drops):
-        super().__init__(health, attack, moveset)
+class ingredient(typeenemy):
+    def __init__(self, name, health, attack, moveset, drops):
+        super().__init__(name, health, attack, moveset)
         self.drops = drops
     def __str__(self):
-        return f"{self.name}, {self.health}, {self.attack}"
+        return f"{self.name}, {self.health}, {self.attack}, {self.moveset}, {self.drops}"
+    
 
+with open("enemy.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    enemy = json.load(f)
+    ##Call classes in here
+
+while True:
+    name = input("Name of enemy: ")
+    health = input("health of enemy: ")
+    attack = input("Attack power: ")
+    moveset = input("moveset: ")
+    drops = input("drops: ")
+    newenemy = ingredient(name, health, attack, moveset, drops)
+    enemy.append(newenemy.__dict__)
+    addanotherenemy = input("Do you want to add another character? Y/N: ")
+    if addanotherenemy.upper() != "Y":
+        break
+
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(enemy)
+
+    #"Write the JSON string to the new JSON file"
+
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("enemy.json")
+os.rename(new_file, "enemy.json")

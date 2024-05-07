2aae16f8240d85e0a44289817ebc6b74b5643993
alexwong0 <alexw87@nycstudents.net>
2024-05-07 12:16:07 -0400

    k

--- enemy.py
+++ enemy.py
@@ -6,8 +6,4 @@ class enemy():
         self.drops = drops
         self.moveset = moveset
 
-class boss(enemy):
-    def __init__(self, name, health, attack, drops, moveset):
-        super().__init__(name, health, attack, drops, moveset)
-    def __str__(self):
-        return f"{self.name}, {self.health}, {self.attack}, {self.drops}, {self.moveset}"
+placeholder = enemy("x", 999, 999, "x", "x")

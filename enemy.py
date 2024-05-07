2993ca625a89e6ae76a047405aa517e4b6db0b76
hongenl <hongenl@nycstudents.net>
2024-05-07 12:26:56 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -10,13 +10,5 @@ Gordon_Ramsey = enemy("Gordan Ramsey", 200, 10, "Supreme Beef Wellington", ["ITS
 
 Uncle_Roger = enemy("Uncle Roger", 150, 8, "MSG", ["MSG", "FUI YOHH", "AI YA"])
 
-
-
-
-
-""" class boss(enemy):
-    def __init__(self, name, health, attack, drops, moveset):
-        super().__init__(name, health, attack, drops, moveset)
-    def __str__(self):
-        return f"{self.name}, {self.health}, {self.attack}, {self.drops}, {self.moveset}" """
+def attack():
     

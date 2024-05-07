220a7378e667a89a1acd41b940b47e2e3ed511ea
alexwong0 <alexw87@nycstudents.net>
2024-05-07 12:28:42 -0400

    Hello

--- enemy.py
+++ enemy.py
@@ -6,13 +6,18 @@ class enemy():
         self.drops = drops
         self.moveset = moveset
 
-placeholder = enemy("name", 200, 200, "drops", ["moveset"])
+enemy_template = enemy("name", 200, 200, ["drops"], ["moveset"])
 
 Gordon_Ramsey = enemy("Gordan Ramsey", 200, 10, "Supreme Beef Wellington", ["ITS RAW!!", "Idiot Sandwich"])
-
+##boss
 Uncle_Roger = enemy("Uncle Roger", 150, 8, "MSG", ["MSG", "FUI YOHH", "AI YA"])
+##boss
 
+Homecook = enemy("Homecook", 20, 5, ["Pepper", "Salt"] ["Struggle Meal Ramen", "Microwaved Cheese Sandwich", "Burnt Fire Alarm"])
 
+NormieChef = enemy("Chef", 50, 10, ["Eggs", "Tomatoes", "Bell Pepper"], ["moveset"])
+
+ProChef = enemy("Advanced Chef", 200, 200, "drops", ["moveset"])
 
 
 

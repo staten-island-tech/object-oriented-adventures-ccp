421b0cbc0dfb53e4749e668559bfaa0a0efd6d8a
hongenl <hongenl@nycstudents.net>
2024-05-07 12:08:20 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,12 +1,13 @@
 class enemy():
-    def __init__(self, name, health, attack):
+    def __init__(self, name, health, attack, drops, moveset):
         self.name = name
         self.health = health
         self.attack = attack
-
-class typeenemy(enemy):
-    def __init__(self, health, attack, drops):
-        super().__init__(health, attack)
         self.drops = drops
+        self.moveset = moveset
+
+class boss(enemy):
+    def __init__(self, name, health, attack, drops, moveset):
+        super().__init__(name, health, attack, drops, moveset)
     def __str__(self):
-        return f"{self.name}, {self.health}, {self.attack}"
+        return f"{self.name}, {self.health}, {self.attack}, {self.drops}, {self.moveset}"

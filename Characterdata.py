3bc3ecc18b2353f5f501d9adf327b92df5e09f0d
AlexZheng82 <alexz82@nycstudents.net>
2024-05-13 11:59:04 -0400

    Hello

--- Characterdata.py
+++ Characterdata.py
@@ -1,9 +1,5 @@
 import random
-step_counter=0
-total_step=0
-weight_chance=(0.5, 0.3, 0.15, 0.05)
-enemyencounter=("none", 0, 1, 2)
-class actionwalking():
+class characterdata():
     def __init__(self, attack, defense, health, rizz, score_mutipler, intellgence, step_counter, total_step, weight_chance, enemyencounter):
         self.attack=attack
         self.defense=defense
@@ -16,17 +12,20 @@ class actionwalking():
         self.weight_chance=weight_chance
         self.enemyencounter=enemyencounter
     def __str__(self):
-        return f"{self.attack}, {self.defense}, {self.health}, {self.rizz}, {self.score_mutipler}, {self.intellgence}, {self}"
-    def walking():
-        total_step+=1
-        encounter=random.choices(enemyencounter, weight_chance)
+        return f"{self.attack}, {self.defense}, {self.health}, {self.rizz}, {self.score_mutipler}, {self.intellgence}, {self.step_counter}, {self.total_step}, {self.weight_chance}, {self.enemyencounter}"
+class action():
+    def walking(x):
+        x.total_step+=1
+        encounter=random.choices(x.enemyencounter, x.weight_chance)
         if not isinstance(encounter, int):
-            if step_counter==10:
+            if x.step_counter==10:
                 while not isinstance(encounter, int):
-                    encounter=random.choices(enemyencounter, weight_chance)
+                    encounter=random.choices(x.enemyencounter, x.weight_chance)
             else:
-                step_counter+=1
+                x.step_counter+=1
         else:
             print(encounter)
-            print(step_counter)
-            print(total_step)
+            print(x.step_counter)
+            print(x.total_step)
+x=characterdata(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+action.walking(x)

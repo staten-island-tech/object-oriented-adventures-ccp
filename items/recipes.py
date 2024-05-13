0afda270f7520f98a30503ede203995b0a5e8a4e
alexwong0 <alexw87@nycstudents.net>
2024-05-13 12:28:48 -0400

    Hello

---
+++ items/recipes.py
@@ -0,0 +1,17 @@
+#Cooks will drop recipes and ingredients, ingredients can be combined by following a recipe to create dishes
+
+class recipe():
+    def __init__(self, name, heal, attack, buff, debuff, ingredients):
+        self.name = name
+        self.heal = heal
+        self.attack = attack
+        self.buff = buff
+        self.debuff = debuff
+        self.ingredients = ingredients
+
+recipe_template = recipe("Name", 10, 10, 10, 10, ["ingredients needed"])
+
+Beef_Wellington = recipe("Beef Wellington", 10, 10, 10, 10, ["B"])
+
+Habanero = recipe("Habanero", 10, 10, 10, 10, ["Bell Pepper"])
+a
---
+++ tutorial.py
@@ -0,0 +1,15 @@
+#one big print with delays between each 
+#for loop 
+import time
+print("This will be a turn-based game.")
+time.sleep(1)
+print("You have ")
+time.sleep(1)
+print("You must venture into various restrauants, and challenge each chef to a duel")
+time.sleep(1)
+print("As you progress into your masterchef journey, you will gather ingredients and recipes dropped by the chefs")
+time.sleep(1)
+print("Ingredients can be used as their own move, but can also be combined by following a recipe to craft meals")
+time.sleep(1)
+print("Meals are stronger than ingredients, and can also have buffs/debuffs")
+time.sleep(1)

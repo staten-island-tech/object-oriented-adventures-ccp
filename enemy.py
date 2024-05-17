7992ccb426ee36633aaaf61d925cd7960e8f9510
hongenl <hongenl@nycstudents.net>
2024-05-17 12:32:31 -0400

    g

--- enemy.py
+++ enemy.py
@@ -7,36 +7,20 @@ class enemy():
         self.drops = drops
         self.moveset = moveset
 
-Boss_Gordon_Ramsey = enemy("Gordan Ramsey", 200, 999, "Supreme Beef Wellington", ["ITS RAW!!", "Idiot Sandwich", "YOU DONKEYYY!!"])
-#Gordon final boss in restrauant(restrauant is biome/level) Hell's Kitchen
-
-Boss_Uncle_Roger = enemy("Uncle Roger", 150, 999, "MSG", ["MSG", "FUI YOHH", "AI YA"])
-
-Boss_Jamal = enemy("Jamal", 100, 50, ["Jerk Chicken", "Fried Chicken", "Watermelon", "Purple Kool-Aid"], 
-['''Tsamina mina, eh, eh
-Waka waka, eh, eh
-Tsamina mina zangalewa
-This time for Africa'''])
-
-Homecook = enemy("Homecook", 20, 5, ["Pepper", "Salt"], ["Struggle Meal Ramen", "Microwaved Cheese Sandwich", "Burnt Fire Alarm"])
-
-NormieChef = enemy("Chef", 50, 10, ["Eggs", "Tomatoes", "Bell Pepper"], ["moveset"])
-
-ProChef = enemy("Advanced Chef", 75, 20, "drops", ["moveset"])
-
 class character():
-    def __init__(self, health, attack, defense):
+    def __init__(self, name, health, attack, defense):
+        self.name = name
         self.health = health
         self.attack = attack
         self.defense = defense
-x = character(200, 20, 20)
+x = character("Sigma Rizzler", 200, 20, 20)
 def enemytype(y):
     Boss_Gordon_Ramsey = enemy("Gordan Ramsey", 200, 999, "Supreme Beef Wellington", ["ITS RAW!!", "Idiot Sandwich", "YOU DONKEYYY!!"])
     
     Boss_Uncle_Roger = enemy("Uncle Roger", 150, 999, "MSG", ["MSG", "FUI YOHH", "AI YA"])
 
     Boss_Jamal = enemy("Jamal", 100, 50, ["Jerk Chicken", "Fried Chicken", "Watermelon", "Purple Kool-Aid"], 
-    ['''Tsamina mina, eh, eh, Waka waka, eh, eh, Tsamina mina zangalewa, This time for Africa'''])
+    ['''Tsamina mina, eh, eh\n Waka waka, eh, eh\n Tsamina mina zangalewa,\n This time for Africa'''])
 
     Homecook = enemy("Homecook", 20, 5, ["Pepper", "Salt"], ["Struggle Meal Ramen", "Microwaved Cheese Sandwich", "Burnt Fire Alarm"])
 
@@ -49,12 +33,20 @@ def enemytype(y):
     elif y == "2":
         return Boss_Uncle_Roger
     elif y =="3":
+        return Boss_Jamal
+    elif y == "4":
+        return Homecook
+    elif y == "5":
+        return NormieChef
+    else:
+        return ProChef
+
 class combat():
     def combating(x, e):
         while x.health > 0 and e.health > 0:
-            print(x.health)
-            print(e.health)
-            player = input("1. Attack\n2. defense\n3.heal")
+            print(x.name, x.health)
+            print(e.name, e.health)
+            player = input("1. Attack\n2. Retreat\n3. Eat\n4. Rizz: ")
             if player == "1":
                 e.health -= x.attack
                 x.health -= e.attack
@@ -70,7 +62,7 @@ class walking():
     def walk():
         player = input("Press 1 to walk: ")
         if player == "1":
-            combat.combating(x, enemytype(random.choices(['1', '2'], (0.1, 0.9))))
+            combat.combating(x, enemytype(random.choices(['1', '2','3','4','5','6'], (0.05,0.05,0.5,0.1,0.1,0.2))))
         else:
             print("<<|Achievement Unlocked: The Special One|>>")
 while True:

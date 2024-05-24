29bed9dd7c33a4699bb38f6daaa364f7cf399c5c
alexwong0 <alexw87@nycstudents.net>
2024-05-24 12:08:06 -0400

    a

--- worlds/worlds.py
+++ worlds/worlds.py
@@ -8,10 +8,13 @@ class restaurants():
 enemytype()
 
 Hells_Kitchen = restaurants('Hells Kitchen', Boss_Gordon_Ramsay , [ProChef])
+#Fake last stage with fake final boss
 
 MSG_Munch = restaurants("MSG Munch", Boss_Uncle_Roger, [Homecook])
+#True last stage with true last boss
 
 Flavor_Town = restaurants("Flavor Town", Boss_Guy_Fieri, [NormieChef, ProChef])
+#1st stage with 1st boss
 
 Jamaica_Fried_Chicken = restaurants("Jamaica Fried Chicken", Boss_Jamal, [NormieChef, JamaicanChef])
-
+#2nd stage and 2nd boss

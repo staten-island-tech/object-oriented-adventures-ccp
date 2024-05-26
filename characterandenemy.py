0be5dc898a45b3ffdfbd7a9ddc9b4c881479918b
AlexZheng82 <alexz82@nycstudents.net>
2024-05-25 20:26:37 -0700

    Hello

---
+++ characterandenemy.py
@@ -0,0 +1,26 @@
+class enemy():
+    def __init__(self, name, health, attack, defense):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense 
+class characterdata(enemy):
+    def __init__(self, attack, defense, health, rizz, score_mutipler, intellgence, total_step, weight_chance, enemyencounter, name):
+        super().__init__(name, health, attack, defense)
+        self.rizz=rizz
+        self.score_mutipler=score_mutipler
+        self.intellgence=intellgence
+        self.total_step=total_step
+        self.weight_chance=weight_chance
+        self.enemyencounter=enemyencounter
+        self.name=name
+    def __str__(self):
+        return f"{self.attack}, {self.defense}, {self.health}, {self.rizz}, {self.score_mutipler}, {self.intellgence}, {self.total_step}, {self.weight_chance}, {self.enemyencounter}, {self.name}"
+    def enemytypestat(worldtype, typenumber):
+        if typenumber=="Boss":
+            if worldtype=="4":
+                Boss_Gordon_Ramsey=enemy("Gordon Ramsey", 200, 999, 30)
+            elif worldtype=="3":
+                Boss_Uncle_Roger=enemy("Uncle Roger", 150, 999, 25)
+            elif worldtype=="2":
+                Boss_Jamal=enemy("Jamal")
--- enemyandcombat.py
+++ enemyandcombat.py
@@ -9,44 +9,11 @@ with open("json/armorstat.json", "r") as f:
 with open("json/itemstatuseffect.json", "r") as f:
     itemstatuseffect=json.load(f)
 #This is the enemy templete
-class enemy():
-    def __init__(self, name, health, attack, drops, moveset):
-        self.name = name
-        self.health = health
-        self.attack = attack
-        self.drops = drops
-        self.moveset = moveset
 #This is the combat system
 class combat():
-#This determine what enemy you encounter and set the enemy stat
-    def enemytype(e):
-        Boss_Gordon_Ramsey = enemy("Gordan Ramsey", 200, 999, "Supreme Beef Wellington", ["ITS RAW!!", "Idiot Sandwich", "YOU DONKEYYY!!"])
-        
-        Boss_Uncle_Roger = enemy("Uncle Roger", 150, 999, "MSG", ["MSG", "FUI YOHH", "AI YA"])
-
-        Boss_Jamal = enemy("Jamal", 100, 50, ["Jerk Chicken", "Fried Chicken", "Watermelon", "Purple Kool-Aid"], 
-        ['''Tsamina mina, eh, eh\n Waka waka, eh, eh\n Tsamina mina zangalewa,\n This time for Africa'''])
-
-        Homecook = enemy("Homecook", 20, 5, ["Pepper", "Salt"], ["Struggle Meal Ramen", "Microwaved Cheese Sandwich", "Burnt Fire Alarm"])
-
-        NormieChef = enemy("Chef", 50, 10, ["Eggs", "Tomatoes", "Bell Pepper"], ["moveset"])
-
-        ProChef = enemy("Advanced Chef", 75, 20, "drops", ["moveset"])
-        if e == "1":
-            return Boss_Gordon_Ramsey
-        elif e == "2":
-            return Boss_Uncle_Roger
-        elif e =="3":
-            return Boss_Jamal
-        elif e == "4":
-            return Homecook
-        elif e == "5":
-            return NormieChef
-        elif e== "6":
-            return ProChef
 #This calculate the amount of damage your enemy take    
-    def damagedealcal(attack, inventory, buff_amount):
-        return attack*weaponstat[0][inventory[2]["Weapon"]]*buff_amount
+    def damagedealcal(attack, inventory, buff_amount, defenseofenemy):
+        return (attack*weaponstat[0][inventory[2]["Weapon"]]*buff_amount)/defenseofenemy
 #This calculate the amount of damage you take    
     def damagetakencalcaltor(enemy_attack, inventory):
         x=[armorstat[0][inventory[2][i]] for i in inventory[2] if not i =="Weapon" if not inventory[2][i]=="none"]
@@ -60,10 +27,10 @@ class combat():
             print(e.name, e.health)
             player = input("1. Attack\n2. Retreat\n3. Use item\n4. Rizz")
             if player == "1":
-                e.health -= combat.damagedealcal(x.attack, buff_amount)
-                x.health -= combat.damagetakencalcaltor(e.attack)
+                e.health -= combat.damagedealcal(x.attack, inventory, buff_amount, e.defense)
+                x.health -= combat.damagetakencalcaltor(e.attack, inventory)
             elif player == "2":
-                x.health -= combat.damagetakencalcaltor(e.attack)*5
+                x.health -= combat.damagetakencalcaltor(e.attack, inventory)*5
                 break
             elif player == "3":
                 number_selection=0
@@ -100,7 +67,7 @@ class combat():
                     break
                 else:
                     print("Rizz failed due to too little rizz you ugly")
-                    x.health -= combat.damagetakencalcaltor(e.attack)*3
+                    x.health -= combat.damagetakencalcaltor(e.attack, inventory)*3
         else:
             if x.health > e.health:
                 print(x.name,x.health)

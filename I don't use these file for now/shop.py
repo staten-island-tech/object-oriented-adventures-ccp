9112e1a20bdf12ad018828b709441f2dee266cc7
hongenl <hongenl@nycstudents.net>
2024-05-30 12:21:59 -0400

    yfguh,jhgf

--- I don't use these file for now/shop.py
+++ I don't use these file for now/shop.py
@@ -13,42 +13,8 @@ class shopitems:
         if buy_item in shop:
             print (test_ingredients)
     else:
-            print("Shop closed")
+            print("Shop closed")"""
 
-
- """
-with open("json/NEWingredients.json", "r") as f:
-# Serialize the updated Python list to a JSON string
-    poop = json.load(f)
-
-class ingredients():
-    def __init__(self, name, price, description, statuseffect):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.statuseffect = statuseffect
-
-
-while True:
-    name = input("Name of the Pokemon: ")
-    price = input("price of the Pokemon: ")
-    description = input("description: ")
-    statuseffect = input("status effect (H - heal, A- attack, B - buff): ")
-    new_pokemon = ingredients(name, price, description, statuseffect)
-    poop.append(new_pokemon.__dict__)
-    addanotherpokemon = input("Do you want to make another pokemon? y/n: ")
-    if addanotherpokemon.upper() != "Y":
-        break
-
-new_file = "updated.json"
-with open(new_file, "w") as f:
-    # Serialize the updated Python list to a JSON string
-    json_string = json.dumps(poop)
-
-    #"Write the JSON string to the new JSON file"
-
-    f.write(json_string)
-
-# Overwrite the old JSON file with the new one
-os.remove("json/NEWingredients.json")
-os.rename(new_file, "json/NEWingredients.json")
+def shop(data):
+    for Data in data:
+        print(Data.)
